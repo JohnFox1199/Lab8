a1b28b475bd6b68c52acf3e6ed8fc7f52332724a
John Fox <johnfoxqiq@gmail.com>
2026-06-14 22:05:14 +0300

    add decorators

--- decorators.py
+++ decorators.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 
-
 class Coffee(ABC):
     @abstractmethod
     def cost(self) -> int:
@@ -17,7 +16,47 @@ class SimpleCoffee(Coffee):
 
     def description(self) -> str:
         return "Coffee"
+
+
+class CoffeeDecorator(Coffee):
+    def __init__(self, coffee: Coffee):
+        self._coffee = coffee
+
+    def cost(self) -> int:
+        return self._coffee.cost()
+
+    def description(self) -> str:
+        return self._coffee.description()
+
+
+class MilkDecorator(CoffeeDecorator):
+    def cost(self) -> int:
+        return super().cost() + 30
+
+    def description(self) -> str:
+        return super().description() + ", milk"
+
+
+class SugarDecorator(CoffeeDecorator):
+    def cost(self) -> int:
+        return super().cost() + 10
+
+    def description(self) -> str:
+        return super().description() + ", sugar"
+
+
+class WhippedCreamDecorator(CoffeeDecorator):
+    def cost(self) -> int:
+        return super().cost() + 50
+
+    def description(self) -> str:
+        return super().description() + ", whipped cream"
+
     
 if __name__ == "__main__":
     coffee = SimpleCoffee()
-    print(coffee)
+    coffee = MilkDecorator(coffee)
+    coffee = SugarDecorator(coffee)
+
+    print(coffee.description())
+    print(coffee.cost())       
